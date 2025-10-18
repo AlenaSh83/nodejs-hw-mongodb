5b99251fcfe5582933b603a14d5a20a2205b330a
Alena <aliona333.com@gmail.com>
2025-10-18 15:00:41 +0200

    add

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ import pino from 'pino-http';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
-import YAML from 'yamljs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import contactsRouter from './routers/contacts.js';
@@ -21,7 +20,6 @@ const __dirname = path.dirname(__filename);
 const setupServer = () => {
   const app = express();
 
- 
   app.use(cors());
   app.use(cookieParser());
   app.use(
@@ -33,7 +31,9 @@ const setupServer = () => {
   );
   app.use(express.json());
 
-  const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8')
+  );
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.get('/', (req, res) => {
